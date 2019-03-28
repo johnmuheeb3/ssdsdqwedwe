64ea4ffd34dc402978885206a4c9cec2d1c02555
johnmuheeb3 <44290389+johnmuheeb3@users.noreply.github.com>
2019-03-28 17:32:19 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,177 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "-"
+
+
+client.on("ready", () => {
+    console.log("ss  | Logged in! Server count: ${client.guilds.size}");
+    client.user.setGame(`cards with The LORD`);
+  });
+
+
+
+  client.on("ready", () => {
+    console.log("ss  | Logged in! Server count: ${client.guilds.size}");
+  });
+
+
+
+  client.on("ready", () => {
+    console.log("ss  | Logged in! Server count: ${client.guilds.size}");
+  });
+
+
+
+  client.on("ready", () => {
+    console.log("ss  | Logged in! Server count: ${client.guilds.size}");
+  });
+
+client.on('message', message => {
+    if(message.content.startsWith(prefix + 'new')) {
+        let args = message.content.split(' ').slice(1).join(' ');
+        let support = message.guild.roles.find("name","Support");
+        let ticketsStation = message.guild.channels.find("name", "TICKETS");
+        if(!args) {
+            return message.channel.send('Please type a subject for the ticket.');
+        };
+                if(!support) {
+                    return message.channel.send('**Please make sure that `Support Team` role exists and it\'s not duplicated.**');
+                };
+            if(!ticketsStation) {
+                message.guild.createChannel("TICKETS", "category");
+            };
+                message.guild.createChannel(`ticket-${message.author.username}`, "text").then(ticket => {
+                    message.delete()
+                        message.channel.send(`Your ticket has been created. [ ${ticket} ]`);
+                    ticket.setParent(ticketsStation);
+                    ticketsStation.setPosition(1);
+                        ticket.overwritePermissions(message.guild.id, {
+                            SEND_MESSAGES: false,
+                            READ_MESSAGES: false
+                        });
+                            ticket.overwritePermissions(support.id, {
+                                SEND_MESSAGES: true,
+                                READ_MESSAGES: true
+                            });
+                                ticket.overwritePermissions(message.author.id, {
+                                    SEND_MESSAGES: true,
+                                    READ_MESSAGES: true
+                                });
+                    let embed = new Discord.RichEmbed()
+                                .setTitle('**New Ticket ,**')
+                                .setColor("#0c0909")
+                                .setThumbnail(`${message.author.avatarURL}`)
+                                .addField('Subject :', args)
+                                .addField('By :', message.author)
+                                .addField('in', `<#${message.channel.id}>`);
+
+                                ticket.sendEmbed(embed);
+                }) .catch();
+    }
+    if(message.content.startsWith(prefix + 'close')) {
+        if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("**# - You dont have enough permissions!**");
+            if(!message.member.hasPermission("ADMINISTRATOR")) return;
+        if(!message.channel.name.startsWith("ticket")) {
+            return;
+        };  
+                let embed = new Discord.RichEmbed()
+                    .setAuthor("Do you really want to close this ticket? Repeat the command to make sure. You have 20 seconds.")
+                    .setColor("RANDOM");
+                    message.channel.sendEmbed(embed) .then(codes => {
+
+                    
+                        const filter = msg => msg.content.startsWith(prefix + 'close');
+                        message.channel.awaitMessages(response => response.content === prefix + 'close', {
+                            max: 1,
+                            time: 20000,
+                            errors: ['time']
+                        })
+                        .then((collect) => {
+                            message.channel.delete();
+                        }) .catch(() => {
+                            codes.delete()
+                                .then(message.channel.send('**Operation has been cancelled.**')) .then((c) => {
+                                    c.delete(4000);
+                                })
+                                    
+                            
+                        })
+
+
+                    })
+
+
+            
+    }
+});
+
+
+client.on('ready', function(){
+    client.user.setStatus("online");
+    var ms = 100000 ;
+    var setGame = [`-new`,`cards with The LORD`,`Premuim Number 0006`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/jojo`);
+    }, ms);100000
+ 
+});
+
+
+
+onst developers = ["348554915876175902" , "349095859859881984"]
+const adminprefix = "-";
+client.on('message', message => {//Toxic Codes
+    var argresult = message.content.split(` `).slice(1).join(' ');//Toxic Codes
+      if (!developers.includes(message.author.id)) return;
+     
+  if (message.content.startsWith(adminprefix + 'setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅ تم تغير اللعبة الى  ${argresult}**`)
+  } else
+     if (message.content === (adminprefix + "leave")) {//Toxic Codes
+    message.guild.leave();   //Toxic Codes
+  } else  
+  if (message.content.startsWith(adminprefix + 'setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});//Toxic Codes
+      message.channel.send(`**✅ تم تغير المشاهدة الى  ${argresult}**`)//Toxic Codes
+  } else
+  if (message.content.startsWith(adminprefix + 'setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅  تم تغير الاستماع الى ${argresult}**`)//Toxic Codes
+  } else
+  if (message.content.startsWith(adminprefix + 'sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/BlueGamer295YT");
+      message.channel.send(`**✅ تم تغير الستريم الى ${argresult}**`)//Toxic Codes
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+  if (message.content.startsWith(adminprefix + 'setprefix')) {//Toxic Codes
+  client.user.setPrefix(argresult).then
+      message.channel.send(`Changing Prefix ..**${argresult}** `)//Toxic Codes
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {//Toxic Codes
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);//Toxic Codes
+}
+});//Toxic Codes
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
